82030319914fe8e6830ddb174c53e6ec8d24b20f
Cristian Douce <cristian@gravityonmars.com>
2015-03-02 17:57:17 -0300

    First test case

---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+
+var TutorialNavigator = require('./index.js');
+
+describe('TutorialNavigator()', function() {
+  before(function(done) {
+    this.tutorial = new TutorialNavigator();
+    done();
+  });
+
+  it('should initialize', function(done) {
+
+  });
+});
